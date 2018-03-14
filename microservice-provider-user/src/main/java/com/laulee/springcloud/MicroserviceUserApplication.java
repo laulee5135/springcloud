9bc8de2acefdfb6e6f1ca6cf5397f4985b10f164
laulee <gslaulee@163.com>
2018-03-14 18:24:29 +0800

    eureka

--- microservice-provider-user/src/main/java/com/laulee/springcloud/MicroserviceUserApplication.java
+++ microservice-provider-user/src/main/java/com/laulee/springcloud/MicroserviceUserApplication.java
@@ -3,11 +3,12 @@ package com.laulee.springcloud;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.autoconfigure.domain.EntityScan;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
+import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
 import org.springframework.context.annotation.ComponentScan;
 
 @SpringBootApplication
-@ComponentScan(value={"com.*"})
-@EntityScan("com.laulee.springcloud.entity")
+@EnableEurekaClient   //只能用于eureka服务发现组件    @EnableDiscoveryClient和@EnableEurekaClient作用一样，但是可以作为eureka注解之外，还能集成zk等服务发现组件
 public class MicroserviceUserApplication {
 
 	public static void main(String[] args) {
--- microservice-provider-user/src/main/java/com/laulee/springcloud/controller/UserController.java
+++ microservice-provider-user/src/main/java/com/laulee/springcloud/controller/UserController.java
@@ -1,12 +1,16 @@
 package com.laulee.springcloud.controller;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.cloud.client.ServiceInstance;
+import org.springframework.cloud.client.discovery.DiscoveryClient;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RestController;
 
 import com.laulee.springcloud.dao.UserRepository;
 import com.laulee.springcloud.entity.User;
+import com.netflix.appinfo.InstanceInfo;
+import com.netflix.discovery.EurekaClient;
 
 @RestController
 public class UserController {
@@ -14,9 +18,27 @@ public class UserController {
 	@Autowired
 	private UserRepository userRepository;
 	
+	@Autowired
+	private EurekaClient eurekaClient;
+	
+	@Autowired
+	private DiscoveryClient discoveryClient;
+	
 	@GetMapping("/simple/{id}")
 	public User findById(@PathVariable Long id){
-		return this.userRepository.findById(id).get();
+		return this.userRepository.findOne(id);
+	}
+	
+	@GetMapping("/eureka-instance")
+	public String serviceUrl(){
+		InstanceInfo instance = this.eurekaClient.getNextServerFromEureka("microservice-provider-user",false);
+		return instance.getHomePageUrl();
+	}
+	
+	@GetMapping("/instance-info")
+	public ServiceInstance showInfo(){
+		ServiceInstance serviceInstance = this.discoveryClient.getLocalServiceInstance();
+		return serviceInstance;
 	}
 	
 }
